02e614f8724895c6f33ca5dbd9e746fda2622b9d
stiffchoclate <abs.ismail08@gmail.com>
2025-07-09 14:25:33 +0100

    Able to play links from youtube.
    Safety in program and links still needs to be built. But that comes last. We are building the skeleton.

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ from nextcord.ext import commands
 import dotenv
 import logging
 import random
-import yt_dlp
+import subprocess
+# import yt_dlp
 
 # Logging
 logger = logging.getLogger('nextcord')
@@ -16,19 +17,38 @@ logger.addHandler(handler)
 dotenv.load_dotenv()
 config = dotenv.dotenv_values()
 token = config['DISCORD_BOT_TOKEN']
-TESTING_GUILD_ID = int(config['TESTING_GUILD_ID'])  # Make sure this is an int
+test_guild_id = config["TESTING_GUILD_ID"]
+assert(token != None)
+assert(test_guild_id != None)
+TESTING_GUILD_ID = int(test_guild_id)  # Make sure this is an int
 
 # Bot & gateway intents setup.
 intents = nextcord.Intents.default()
 bot = commands.Bot(intents=intents)
 
 #Assign variable for the channel connection.
-botVoiceClient: nextcord.VoiceClient | None = None
+botVoiceClient: nextcord.VoiceClient = None
 
+#======================== Intitial Commands ================================#
 @bot.event
 async def on_ready():
     #Load media files from elsewhere
+    connections = bot.voice_clients
+
+    for vc in connections:
+        await vc.disconnect(force=True)
+    
     print(f'We have logged in as {bot.user}')
+
+# When bot goes offline and completely disconnects from discord.
+@bot.event
+async def on_disconnect():
+    connections = bot.voice_clients
+
+    for vc in connections:
+        await vc.disconnect(force=True)
+
+    print("The bot disconnected fr. Should also disconnect voice connections everywhere.\n")
 
 
 # roll command 
@@ -46,46 +66,141 @@ async def roll(
     await interaction.send(f"🎲 You rolled a **{result}** (from {minimum} to {maximum})")
 
 
+#======================== Connection Commands & Logic ================================#
+
 @bot.slash_command(name="join", description="Join current voice channel", guild_ids=[])
 async def join(interaction: nextcord.Interaction):
-    global botVoiceClient
+    await interaction.response.defer(ephemeral=True,with_message=True)
 
     #identify user voice state
+    botVoiceClient: nextcord.VoiceClient | None = interaction.guild.voice_client # type: ignore
     userVoiceState = interaction.user.voice
-    if (userVoiceState == None):
-        await interaction.send("not in a voice channel")
-    else:
-        channel = interaction.user.voice.channel
-        botVoiceClient = await channel.connect(timeout = 2.0)
-        assert(botVoiceClient != None)
 
-        await interaction.send("should join now")
+    if (userVoiceState == None):
+        await interaction.followup.send("not in a voice channel")
+    elif (botVoiceClient and botVoiceClient.channel == userVoiceState.channel):
+        # What if you don't want users to move the bot relentessly
+        await interaction.followup.send("bot already connected to this channel") 
+    else:
+        channel = userVoiceState.channel
+        # I think <timeout> controls how long it takes for the bot to disconnect due to inactivity.
+        await channel.connect(reconnect=True) 
+        await interaction.followup.send("should join now")
 
 @bot.slash_command(name="leave", description="Leave the current voice channel.", guild_ids=[])
 async def leave(interaction: nextcord.Interaction):
-    global botVoiceClient
-    
+    vc = interaction.guild.voice_client
     channel = interaction.user.voice.channel
     if (channel.type != None):
-        await botVoiceClient.disconnect(force=True)
-        botVoiceClient = None
+        await vc.disconnect(force=True)
         await interaction.send("bot has left")
     else:
-        await interaction.send("you are not in a voice channel")
+        await interaction.send("you are not in a voice channel") 
 
-@bot.slash_command(description="tests for soundcloud fetching", guild_ids=[])
-async def test_ytdlp_play(interaction: nextcord.Interaction):
+"""
+def sameBotUserVoiceChannel(func):
+    async def wrapper(interaction: nextcord.Interaction, *args, **kwargs):
+        global botVoiceClient
+        
+        # gonna really lock in tomorrow
+        member = interaction.guild.get_member(interaction.user.id) # pylance errors due to lack of null checks and what not. could be dangerous ma boi. This is where the errors can start to pile up if I'm not careful
+        userVC = interaction.user.voice.channel
+        if userVC==None:
+            await interaction.send("User isn't in a voice channel")
+        elif botVoiceClient.is_connected() and botVoiceClient.
+
+    return wrapper
+"""
+
+# Called when a stream ends or an error occurs.
+# can i pass an interaction? can I edit// override the function to take the interactionm
+async def streamEndsOrError(error):
     global botVoiceClient
-    await interaction.send("playing sc audio")
+    print("stream ended or there was an error trying to play the audio")
+    await botVoiceClient.disconnect()
+    botVoiceClient = None
+
+#======================== Initial Playback Commands ================================#
+
+def get_audio_subprocess(form: str, url: str):
+    completed_process = subprocess.run(
+        args=['yt-dlp.exe', '-x', '-g', '--audio-format', form, url],
+        capture_output=True,
+        text=True
+    )
+    return completed_process
+
+@bot.slash_command(description="tests for YTDL audio fetching", guild_ids=[])
+async def test_ytdlp_play(
+    interaction: nextcord.Interaction,
+    url: str = nextcord.SlashOption(description="YT URL"),
+):
+    # Check for bot being joined already.
+    guild: nextcord.Guild | None = interaction.guild
+    if guild == None or guild.id != TESTING_GUILD_ID:
+        await interaction.send("incorrect guild//server origin for this command")
+        return
+    
+    vc: nextcord.VoiceClient | None = guild.voice_client # type: ignore
+    if vc == None:
+        await interaction.send("bot hasn't joined channel")
+        return
+    
+    await interaction.response.defer(ephemeral=True,with_message=True) # Make user wait for response
+
+    # Try get OPUS url.
+    audio_url: str | None = None
+    source: nextcord.FFmpegOpusAudio | nextcord.FFmpegPCMAudio | None = None 
+    try: # Opus URL
+        process = get_audio_subprocess('opus', url)
+        process.check_returncode()
+        audio_url = process.stdout
+        source = nextcord.FFmpegOpusAudio(source=audio_url)
+
+    # Could these calls to get_audio_process be done asyncronosly.
+    except subprocess.CalledProcessError: # Alternative enconding URLs
+        forms = ['best', 'aac', 'alac', 'flac', 'm4a', 'mp3', 'vorbis','wav']
+        for form in forms:
+            process = get_audio_subprocess(form=form, url=url)
+
+            try: # Check for successful extraction
+                process.check_returncode()
+                audio_url = process.stdout
+                print(f'The url is {audio_url}')
+                source = nextcord.FFmpegPCMAudio(source=audio_url)
+                break # exit for loop
+
+            except subprocess.CalledProcessError:
+                print("yt_dlp command failed.")
+                continue
+            except nextcord.ClientException:
+                print('FFmpegPCMAudio object creation failed')
+                continue
+            except:
+                print('unexpected error occured')
+                continue
+
+            # Could be multiple types of exceptions occuring
+
+    # Could be multiple types of exceptions occuring
+    except:
+        print('unknown error occured during Opus URL acquirement')
+    
+    finally:
+        if audio_url and source:
+            vc.play(source=source, after=streamEndsOrError)
+            print(f'source type: {type(source)}')
+            await interaction.followup.send("playing audio now")
+        else:
+            await interaction.followup.send("badeni couldn't process your request") 
 
 
 # Plays a song at a given link. Runs through entire song. Only works for local files
-# link: String = nextCord.SlashOption(description="link")
-
-@bot.slash_command(description="Testing for attempt to play audio successfully", guild_ids=[TESTING_GUILD_ID])
+@bot.slash_command(description="Testing for attempt to play (specific) local audio file successfully", guild_ids=[TESTING_GUILD_ID])
 async def test_play(interaction: nextcord.Interaction):
     #identify user and channel.
     global botVoiceClient
+    print("Running function")
     
     userChannel = interaction.user.voice.channel
     userVoice = interaction.user.voice
@@ -101,10 +216,11 @@ async def test_play(interaction: nextcord.Interaction):
         # Create audio source
         fp = r'C:\Users\Abdul\OneDrive\Documents\Discord Bots\stiff\media\tester.mp3'
         fp2 = r'C:\Users\Abdul\OneDrive\Documents\Discord Bots\stiff\media\Skepta x Smooth Soul 2 [K5Ywq18TeVA].webm'
-        with open(fp2, 'rb') as file:
-            source = nextcord.FFmpegPCMAudio(source=fp2)
-            botVoiceClient.play(source=source, after=streamEndsOrError)
-            await interaction.send("planning to play audio")
+        fp3 = r'C:\Users\Abdul\OneDrive\Documents\Discord Bots\stiff\media\opus_skeppy.opus'
+
+        source = nextcord.FFmpegOpusAudio(fp3)
+        botVoiceClient.play(source=source, after=streamEndsOrError)
+        await interaction.send("planning to play audio")
     else:
         await interaction.send("wrong channel buddy")
     # Fetch the data to be played.
@@ -120,17 +236,10 @@ async def test_pause(interaction: nextcord.Interaction):
 @bot.slash_command(description="Pauses badeni", guild_ids=[TESTING_GUILD_ID])
 async def test_resume(interaction: nextcord.Interaction):
     global botVoiceClient
+    
     botVoiceClient.resume()
     await interaction.send("resuming now")
-
     
-    
-# Called when a stream ends or an error occurs.
-async def streamEndsOrError(error):
-    global botVoiceClient
-    await botVoiceClient.disconnect()
-    botVoiceClient = None
-
 
 # hello command
 @bot.slash_command(description="My first slash command.", guild_ids=[])
@@ -138,4 +247,7 @@ async def hello(interaction: nextcord.Interaction):
     await interaction.send("Hello!")
 
 # Run bot
-bot.run(token)
+if token == None:
+    print("ERROR: no bot token found")
+else:
+    bot.run(token)
